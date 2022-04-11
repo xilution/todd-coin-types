f74c77454a24001a7ee04effe1922d728d44c4c6
Todd Brunia <tbrunia@gmail.com>
2022-04-11 08:19:00 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+export interface ParticipantKey {
+  public: string;
+  private?: string;
+}
+
+export enum Roles {
+  VOLUNTEER = "VOLUNTEER",
+  CHARITY = "CHARITY",
+  NODE = "NODE",
+}
+
+export interface Participant {
+  id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  key: ParticipantKey;
+  roles?: Roles[];
+}
+
+export interface Transaction {
+  id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  from?: string;
+  to: string;
+  amount: number;
+  description: string;
+  signature?: string;
+}
+
+export interface Block {
+  id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  transactions: Transaction[];
+  nonce: number;
+  previousHash: string;
+  hash: string;
+}
+
+export interface Node {
+  id?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  baseUrl: string;
+}
