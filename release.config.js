e836413c85ac0e6b51e11ee5fade7fa9078fa6b3
Todd Brunia <tbrunia@gmail.com>
2022-04-15 08:26:54 -0500

    chore: added git pre-push hook and cleaned up semantic release

--- release.config.js
+++ release.config.js
@@ -10,8 +10,7 @@ module.exports = {
       "@semantic-release/git",
       {
         assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        message: "chore(release): ${nextRelease.version}",
       },
     ],
     "@semantic-release/github",
