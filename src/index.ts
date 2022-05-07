0c814776fe2a0faedbb352a9a7e865bafcd72d47
Todd Brunia <tbrunia@gmail.com>
2022-05-07 09:58:39 -0500

    feat: added participant key id to signed transaction

--- src/index.ts
+++ src/index.ts
@@ -159,6 +159,7 @@ export interface SignedTransaction<T extends TransactionDetails>
   extends PendingTransaction<T> {
   goodPoints: number;
   signature: string;
+  participantKeyId: string;
 }
 
 export interface BlockTransaction<T extends TransactionDetails>
